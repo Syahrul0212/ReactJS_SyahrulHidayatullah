f0887ded460dbd842a8037b7f1a01ebaad22460e
Syahrul02 <syhrl44@gmail.com>
2023-03-02 02:44:19 +0700

    push js

---
+++ 10_Javascript Refreshment/Pratikum/script.js
@@ -0,0 +1,50 @@
+document.getElementById("submit-button").addEventListener("click", function () {
+    var productName = document.getElementById("pName").value;
+    var productCategory = document.getElementById("pCategory").value;
+    var imageProduct = document.getElementById("imageCategory").value;
+    var productFreshness = document.querySelector(
+      'input[name="productfreshness"]:checked'
+    );
+    var additionalDescription = document.getElementById(
+      "additionaldescription"
+    ).value;
+    var productPrice = document.getElementById("pprice").value;
+  
+    if (productName == "" || productPrice == "") {
+      alert("Please enter a valid Product name and price.");
+    } else {
+      if (productName.length > 25) {
+        alert("Product Name must not exceed 25 characters.");
+      }
+    }
+  
+    if (productName === "") {
+      alert("The Product Name field must be filled in.");
+      return;
+    } else if (/[**@/#{}**]/.test(productName)) {
+      alert("Name must not contain symbols.");
+      return;
+    }
+  
+    if (productCategory === "") {
+      alert("The Product Category field must be filled in.");
+      return;
+    }
+    if (imageProduct === "") {
+      alert("The Image of Product field must be filled in.");
+      return;
+    }
+    if (!productFreshness) {
+      alert("The Product Freshness field must be filled in.");
+      return;
+    }
+    if (additionalDescription === "") {
+      alert("The Additional Description field must be filled in.");
+      return;
+    }
+    if (productPrice === "") {
+      alert("The Product Price field must be filled in.");
+      return;
+    }
+
+});
---
+++ 10_Javascript Refreshment/README.md
@@ -0,0 +1,7 @@
+# 3 point penting javascript
+
+## 1. JavaScript adalah bahasa pemrograman tingkat tinggi yang diinterpretasikan dan digunakan terutama untuk membuat halaman web yang dinamis dan interaktif. Biasanya, JavaScript digunakan bersama HTML dan CSS untuk menambahkan fungsionalitas dan interaktifitas pada sebuah situs web.
+
+## 2. Salah satu fitur paling powerful dari JavaScript adalah kemampuannya untuk memanipulasi Model Objek Dokumen (DOM) dari sebuah halaman web. Dengan JavaScript, kamu dapat memodifikasi konten, styling, dan perilaku dari sebuah halaman web secara dinamis berdasarkan input pengguna atau event lainnya.
+
+## 3. JavaScript adalah bahasa yang sangat fleksibel dan dapat digunakan tidak hanya dalam pengembangan web tetapi juga dalam konteks lain seperti scripting sisi server, pengembangan aplikasi desktop dan mobile, dan bahkan pengembangan game. Popularitas dan penggunaan yang luas telah mengarah pada pengembangan banyak framework dan library yang powerful dan memudahkan untuk membangun aplikasi yang kompleks dengan cepat dan efisien.
