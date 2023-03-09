a030609640a16b4aa0eda60ad4d1b7621a8270cc
Syahrul02 <syhrl44@gmail.com>
2023-03-10 01:43:47 +0700

    push 14 new

---
+++ 14_React Fundamental/Pratikum/createproduct-reactjs/src/component/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <div className="text-center">
+        <img
+          src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png"
+          className="img-fluid mx-auto d-block"
+          alt="Logo Bootstrap"
+          width="
+      71.53px"
+        />
+        <h2>Create Product</h2>
+        <br />
+        <p className="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a </p>
+        <p className="lead">validation state that can be triggered by attempting to submit the form without completing it.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
