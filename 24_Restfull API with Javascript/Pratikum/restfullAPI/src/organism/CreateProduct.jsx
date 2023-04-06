0d9765091706b2664a5dba8bc8517a93ef9153f5
Syahrul02 <syhrl44@gmail.com>
2023-04-07 03:25:48 +0700

    push

---
+++ 24_Restfull API with Javascript/Pratikum/restfullAPI/src/organism/CreateProduct.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Form, Hero, Navbar } from '../component'
+
+function CreateProduct() {
+   
+  return (
+    <div>
+        <Navbar/>
+        <Hero/>
+        <Form/>
+    </div>
+  )
+}
+
+export default CreateProduct;
